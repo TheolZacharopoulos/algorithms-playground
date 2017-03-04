af61250f63f1f832efdb35bdea94604f55a8e9e3
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2017-03-04 20:15:20 +0200

    2 stacks queue (FIX)

--- stacks_queues/two_stacks_queue.py
+++ stacks_queues/two_stacks_queue.py
@@ -5,29 +5,42 @@
 # Always dequeue from dequeue_stack (point 3 if empty).
 # Transfer all elements from enqueue_stack to dequeue_stack if dequeue_stack is empty (if need to dequeue).
 
-dequeue_stack, enqueue_stack = [], []
 
-for _ in range(int(input())):
-    val = list(map(int, input().split()))
+class MyQueue(object):
+    def __init__(self):
+        self.dequeue_stack = []
+        self.enqueue_stack = []
+        self.first = None
 
-    first = None
-
-    if val[0] == 1:
-        # if it is the first enqueue, keep the first value
-        if len(enqueue_stack) == 0:
-            first = val[1]
-
-        # push the value
-        enqueue_stack.append(val[1])
-
-    elif val[0] == 2:
-        if len(dequeue_stack) == 0:
-            while len(enqueue_stack) > 0:
-                dequeue_stack.append(enqueue_stack.pop())
-        dequeue_stack.pop()
-
-    else:
-        if len(dequeue_stack) > 0:
-            print(dequeue_stack[-1])
+    def peek(self):
+        if len(self.dequeue_stack) > 0:
+            return self.dequeue_stack[-1]
         else:
-            print(first)
+            return self.first
+
+    def put(self, value):
+        if len(self.enqueue_stack) == 0:
+            self.first = value
+        self.enqueue_stack.append(value)
+
+    def pop(self):
+        if len(self.dequeue_stack) == 0:
+            while len(self.enqueue_stack) > 0:
+                self.dequeue_stack.append(self.enqueue_stack.pop())
+
+        self.dequeue_stack.pop()
+
+
+queue = MyQueue()
+
+t = int(input())
+for line in range(t):
+    values = map(int, input().split())
+    values = list(values)
+    if values[0] == 1:
+        queue.put(values[1])
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print(queue.peek())
+
