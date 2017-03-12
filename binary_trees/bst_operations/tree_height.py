6a2ada96546f6965a7f495b90bbe9fe7f64fa719
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2017-03-12 20:48:14 +0200

    Small fix

--- binary_trees/bst_operations/tree_height.py
+++ binary_trees/bst_operations/tree_height.py
@@ -11,6 +11,30 @@ def height(root):
         return 1 + max(height(root.get_left()), height(root.get_right()))
 
 
+def height_iterative(root):
+    if root is None:
+        return 0
+
+    node_stack = [root]
+    depth_stack = [1]
+    max_height = 0
+
+    while len(node_stack) > 0:
+        node = node_stack.pop()
+        depth = depth_stack.pop()
+
+        max_height = max_height if max_height > depth else depth
+
+        if node.get_left() is not None:
+            node_stack.append(node.get_left())
+            depth_stack.append(depth + 1)
+
+        if node.get_right() is not None:
+            node_stack.append(node.get_right())
+            depth_stack.append(depth + 1)
+
+    return max_height
+
 r = BSTNode(5,
             BSTNode(3,
                     BSTNode(2),
@@ -21,3 +45,4 @@ r = BSTNode(5,
                     BSTNode(8)))
 
 print(height(r))
+print(height_iterative(r))
