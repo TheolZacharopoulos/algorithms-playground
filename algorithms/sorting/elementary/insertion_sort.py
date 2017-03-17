83c437262232a0101ca9696dada3546b11e530c0
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2017-03-17 19:01:26 +0200

    Insertions and selection sort

---
+++ algorithms/sorting/elementary/insertion_sort.py
@@ -0,0 +1,39 @@
+# Move an index i from left to right
+# in the i'th iteration, swap a[i] with each larger entity to its left
+#
+# Invariants:
+# - entries to the left including the pointer are in ascending order
+# - entries to the right have not been seen yet
+#
+# [1/4 * N^2] -> O(N^2) -> randomly order
+# It does depend on the initial order of the data:
+# - Best case:  the array is ordered => no exchanges just n-1 compares (Faster than selection sort)
+# - Worst case: the array is in descending order => 1/2 * N^2 compares and 1/2 * N^2 exchanges
+#   (Slower than selection sort) same compares but much more exchanges
+#
+# Interesting: Linear time for partially sorted arrays!
+#
+# Stable
+
+
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+
+def insertion_sort(array):
+    # move th pointer to the right
+    for i in range(len(array)):
+
+        # put it on the right order
+        for j in range(i, 0, -1):
+            if array[j] < array[j - 1]:
+                swap(array, j, j-1)
+            else:
+                break
+
+    return array
+
+
+print(insertion_sort([5, 4, 2, 1, 3]))
---
+++ algorithms/sorting/elementary/selection_sort.py
@@ -0,0 +1,64 @@
+# In the i'th iteration find the smallest remaining entry to the bigger index of i and swap that with i.
+# after each swap the left of the i is always in order.
+#
+# while i less than the array's length:
+#     find the smallest
+#     swap with i
+#     increment i
+#
+# Invariants:
+# - Entries to the left never change
+# - No entry to the right is smaller than any element on the left
+#
+# [1/2 * N^2] -> O(N^2) time (even if the input is sorted)
+# Linear time of exchanges, every item is put into its final position with just 1 exchange
+#
+# NOT Stable
+
+
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
+
+def selection_sort(array):
+    for i in range(len(array)):
+        min_pos = i
+
+        for j in range(i+1, len(array)):
+
+            # find the smaller
+            if array[j] < array[min_pos]:
+                min_pos = j
+
+        # swap i with the smallest
+        swap(array, i, min_pos)
+
+    return array
+
+
+def selection_sort_recursive_wrap(array):
+    return selection_sort_recursive(array, 0)
+
+
+def selection_sort_recursive(array, start):
+    if start < len(array)-1:
+        min_pos = start
+
+        # find min
+        for j in range(start, len(array)):
+            if array[j] < array[min_pos]:
+                min_pos = j
+
+        # swap
+        swap(array, start, min_pos)
+
+        # recursive (tail recursion)
+        return selection_sort_recursive(array, start + 1)
+    else:
+        return array
+
+
+print(selection_sort([5, 4, 2, 1, 3]))
+print(selection_sort_recursive_wrap([5, 4, 2, 1, 3]))
