c8d4d1363f273e3d0894a91f9c891b3336e08591
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2017-03-23 12:58:43 +0200

    Start with quicksort

---
+++ algorithms/sorting/not_elementary/quick_sort/quick_sort_queue.py
@@ -0,0 +1,62 @@
+from data_structures.stacks_queues.queues.linked_queue import LinkedQueue
+
+# Time complexity:
+#  - n for each divide and concatenation
+#  - n-1 in worst case (when the array is sorted) for divide on pivot
+
+# Therefore: O(n^2) in worst case.
+# Paradoxically, if we choose the pivot as the last element of the sequence, this worst-case behavior occurs
+# for problem instances when sorting should be easy—if the sequence is already sorted.
+# Solution: Pick a random pivot
+
+# Best case:
+# - L, E, G be almost similar size: then O(n log n)
+
+
+def quick_sort(a_queue):
+    # list is already sorted
+    if len(a_queue) < 2:
+        return
+
+    # using first as arbitrary pivot
+    pivot = a_queue.first()
+
+    lower = LinkedQueue()
+    eq = LinkedQueue()
+    greater = LinkedQueue()
+
+    # divide S into L, E, and G
+    while not a_queue.is_empty():
+        if a_queue.first() < pivot:
+            lower.enqueue(a_queue.dequeue())
+        elif pivot < a_queue.first():
+            greater.enqueue(a_queue.dequeue())
+        else:
+            eq.enqueue(a_queue.dequeue())  # a_queue.first() must equal pivot
+
+    # conquer
+    quick_sort(lower)  # sort elements less than p
+    quick_sort(greater)  # sort elements greater than p
+
+    # concatenate results
+    while not lower.is_empty():
+        a_queue.enqueue(lower.dequeue())
+
+    while not eq.is_empty():
+        a_queue.enqueue(eq.dequeue())
+
+    while not greater.is_empty():
+        a_queue.enqueue(greater.dequeue())
+
+
+if __name__ == "__main__":
+    arr = LinkedQueue()
+    arr.enqueue(5)
+    arr.enqueue(3)
+    arr.enqueue(1)
+    arr.enqueue(6)
+    arr.enqueue(4)
+
+    print(arr)
+    quick_sort(arr)
+    print(arr)
